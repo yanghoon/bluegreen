af76401939f1c93e19634a2a32a9fbc83ae9a829
ji-gyoomin <earth1223@sk.com>
2018-05-04 17:21:28 +0900

    blue application

---
+++ src/main/java/com/sk/tbd/bluegreen/BluegreenApplication.java
@@ -0,0 +1,20 @@
+package com.sk.tbd.bluegreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BluegreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluegreenApplication.class, args);
+	}
+	
+	@GetMapping("/home") 
+	public String home() {
+		return "{\"app\":\"Bluegreen\",\"data\":\"blue\"}";
+	}
+}
